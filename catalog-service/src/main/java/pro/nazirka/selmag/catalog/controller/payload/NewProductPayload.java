7a66329175613858f8822d4f8fce9fc8be142c19
Khashiev <nxashiev@gmail.com>
2024-12-05 12:39:04 +0300

    create rest-api

---
+++ catalog-service/src/main/java/pro/nazirka/selmag/catalog/controller/payload/NewProductPayload.java
@@ -0,0 +1,13 @@
+package pro.nazirka.selmag.catalog.controller.payload;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record NewProductPayload(
+        @NotNull(message = "{catalog.products.create.errors.title_is_null}")
+        @Size(min = 3, max = 30, message = "{catalog.products.create.errors.title_size_is_invalid}")
+        String title,
+
+        @Size(max = 1024, message = "{catalog.products.create.errors.details_size_is_null}")
+        String details) {
+}
